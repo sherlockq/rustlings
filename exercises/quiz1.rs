ba4a18b76d5067a288365cf3cf1a191fa1d88cea
sherlock <615526+sherlockq@users.noreply.github.com>
2022-11-11 15:43:49 +0800

    doing

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,11 +10,18 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(num:u32) -> u32 {
+    const PRICE_PER_APPLE:u32 = 2;
+    const PRICE_PER_APPLE_OVER_40:u32 = 1;
 
+    if num > 40 {
+        PRICE_PER_APPLE_OVER_40 * num
+    } else {
+        PRICE_PER_APPLE * num
+    }
+}
 // Don't modify this function!
 #[test]
 fn verify_test() {
